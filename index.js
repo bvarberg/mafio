9a3a6f30d6a5cb1997d8e496b746fcdf4abd2978
Byron Varberg <byron@byronvarberg.com>
2019-01-12 21:05:58 -0600

    WIP: Clean up extraneous log event listener

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ const config = require('./config');
 const main = async () => {
   try {
     const server = await Mafio.createServer(config);
-
-    server.events.on('log', (event, tags) => {
-      console.log(event);
-    });
-
     await server.start();
-    // console.log(`Mafio server running at: ${server.info.uri}`);
   } catch (err) {
     console.error(err);
     process.exitCode = 1;
